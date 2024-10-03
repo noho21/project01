1c9169e90ec7b3504ad0ae04176c39375ef798ca
hono21 <0521hono@gmail.com>
2024-10-03 17:21:04 +0900

    コード修正

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -18,7 +18,13 @@ class ProductController extends Controller
         $companies = Company::all();
         $product_name = $request -> input('product_name');
         $company_id = $request -> input('company_id');
-        $products = Product::where('product_name', 'like', "%product_name%") -> where('company_id', 'like', "%company_id%") -> paginate(2);
+
+        $products = Product::join('companies', 'products.company_id', '=', 'companies_id')
+        -> select('products.*', 'companies.name as company_name') 
+        -> where('product_name', 'like', "%product_name%")
+        -> where('company_id', 'like', "%$company_id%")
+        -> paginate(2);
+
         return view ('product.index', [
             'products' => $products,
             'companies' => $companies,
