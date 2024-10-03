b28d4ee87547543c3ac0510238bbbfeca07136ed
hono21 <0521hono@gmail.com>
2024-10-03 17:47:45 +0900

    products_table.phpのcompany_id削除

--- database/migrations/2024_09_03_144804_create_products_table.php
+++ database/migrations/2024_09_03_144804_create_products_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table -> id();
             $table -> string('product_name');
-            $table -> unsignedBigInteger('company_id');
             $table -> integer('price');
             $table -> integer('stock');
             $table -> text('comment');
--- database/migrations/2024_10_03_083942_add_company_id_to_products_table.php
+++ database/migrations/2024_10_03_083942_add_company_id_to_products_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::table('products', function (Blueprint $table) {
-            table -> unsignBigInteger('company_id') -> nulable();
+            $table -> unsignedBigInteger('company_id') -> nullable();
         });
     }
 
