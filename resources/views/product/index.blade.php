4e16625c6bea1e491c1a1fb0b3f9f75c29a5276d
hono21 <0521hono@gmail.com>
2024-10-05 00:08:06 +0900

    商品一覧画面をテーブルへ変更

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -8,7 +8,7 @@
                 <form method="GET" action="{{ route('product.index') }}">
                     @csrf
                     
-                    <input type="text" name="product_name" placeholder="表品名">
+                    <input type="text" name="product_name" placeholder="商品名">
                     <select name="company_id">
                         @foreach ($companies as $company)
                             <option value="{{ $company->id }}">{{ $company->company_name }}</option>
@@ -17,30 +17,43 @@
                     <input type="submit" value="検索">
                 </form>
             </div>
-            <ul>
-                @foreach ($products as $product)
-                    <li>
-                        <div>{{ $product -> id }}</div>
-                        <img style="height: 100px; width: 100px;" src="{{ route('product.getfile', ['id' => $product -> id]) }}" alt="商品画像">
-                        <div>{{ $product -> product_name }}</div>
-                        <div>{{ $product -> price }}</div>
-                        <div>{{ $product -> stock }}</div>
-                        <div>{{ $product -> company -> company_name ?? 'デフォルトの会社名' }}</div>
-                        <a href="{{ route('product.show', ['id' => $product -> id]) }}">詳細</a>
-
-                        <form action="{{ route('product.delete') }}" method="post">
-                            @csrf
-                            <input type="hidden" name="id" value="{{ $product -> id }}">
-                            <input type="submit" value="削除">
-                        </form>
-                    </li>
-                @endforeach
-            </ul>
+            <table class="products_table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>商品画像</th>
+                        <th>商品名</th>
+                        <th>価格</th>
+                        <th>在庫数</th>
+                        <th>メーカー名</th>
+                        <th><a href="{{ route('product.new') }}">新規登録</a></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($products as $product)
+                        <tr>
+                            <td>{{ $product -> id }}</td>
+                            <td><img style="height: 100px; width: 100px;" src="{{ route('product.getfile', ['id' => $product -> id]) }}" alt="商品画像"></td>
+                            <td>{{ $product -> product_name }}</td>
+                            <td>{{ $product -> price }}</td>
+                            <td>{{ $product -> stock }}</td>
+                            <td>{{ $product -> company -> company_name ?? 'デフォルトの会社名' }}</td>
+                            <td>
+                                <a href="{{ route('product.show', ['id' => $product -> id]) }}">詳細</a>
+                                <form action="{{ route('product.delete') }}" method="post">
+                                    @csrf
+                                    <input type="hidden" name="id" value="{{ $product -> id }}">
+                                    <button type="submit" value="削除">削除</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
             <div>
                 {{ $products -> links('vendor.pagination.bootstrap-4') }}
             </div>
             <hr>
-            <a href="{{ route('product.new') }}">新規登録</a>
         @endsection
     </body>
 </html>
