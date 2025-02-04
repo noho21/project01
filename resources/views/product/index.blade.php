57279c5fcf5e00a28e16f86aec70d6d87a3fe2e8
hono21 <0521hono@gmail.com>
2025-02-04 23:02:27 +0900

    検索非同期処理のためのJS編集

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -7,9 +7,9 @@
 @section('content')
     <div class="w-1/2 mx-auto mt-4">
     <h3 class="text-left text-2xl w-50 py-5 m-auto">商品一覧画面</h3>
-        <form class="space-x-4 flex justify-center" method="GET" action="{{ route('product.index') }}">
-            <input class="w-2/6 h-10 rounded border-2" type="text" name="product_name" placeholder="検索キーワード">
-            <select class="w-2/6 h-10 rounded border-2 text-center" name="company_id">
+        <form id="searchForm" class="space-x-4 flex justify-center" method="GET" action="{{ route('product.index') }}">
+            <input id="searchKeyword" class="w-2/6 h-10 rounded border-2" type="text" name="product_name" placeholder="検索キーワード">
+            <select id="searchCompany" class="w-2/6 h-10 rounded border-2 text-center" name="company_id">
                     <option value="">全てのメーカー</option>
                     @foreach ($companies as $company)
                         <option value="{{$company->id}}" @selected(request('company_id') == $company->id)>
@@ -18,11 +18,11 @@
                     @endforeach
             </select>
             
-            <input class="w-1/6 h-10 bg-gray-300 outline-offset-2 focus:ring-2 shadow-xl rounded-md" type="submit" value="検索">
+            <button id="searchButton" class="w-1/6 h-10 bg-gray-300 outline-offset-2 focus:ring-2 shadow-xl rounded-md">検索</button>
         </form>
     </div>
 
-    <table class="text-base table-fixed border-4 w-1/2 mx-auto mt-2">
+    <table id="productTable" class="text-base table-fixed border-4 w-1/2 mx-auto mt-2">
         <thead class="border-2 h-14 text-center bg-cyan-500">
             <tr>
                 <th>ID</th>
@@ -36,7 +36,7 @@
                 </th>
             </tr>
         </thead>
-        <tbody>
+        <tbody id="tableBody">
             @foreach ($products as $product)
             <tr class="border-2 h-40 text-center">
                 <td>{{$product->id}}</td>
@@ -60,7 +60,7 @@
                         <form action="{{route('product.delete', ['id' => $product->id])}}" method="POST" data-id="{{ $product->id }}">
                             @csrf
                             <input type="hidden" name="id" value="{{$product->id}}">
-                            <button class="py-2 px-3 bg-red-500 rounded-md text-white" type="button" onclick="deleteProduct(this)">削除</button>
+                            <button id="deleteButton" class="py-2 px-3 bg-red-500 rounded-md text-white" type="button" onclick="deleteProduct(this)">削除</button>
                         </form>
                     </div>
                 </td>
@@ -88,6 +88,7 @@
 
 @section('scripts')
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
     <script>
     function deleteProduct(button) {
         const productId = button.closest('form').dataset.id;
@@ -106,5 +107,50 @@
             }
         });
     }
+    </script>
+    <script>
+    $(document).ready(function() {
+        $('#searchForm').submit(function(event) {
+            event.preventDefault(); //フォーム送信を防ぐ
+            fetchProducts(); // 検索実行
+        });
+
+        var keyword = $('#searchKeyword').val();
+        var company_id = $('##searchCompany').val();
+        $(document).on('click', '.pagination a', function(event) {
+            event.preventDefault();
+            var url = $(this).attr('href');
+            fetchProducts(url);
+        });
+
+        function fetchProducts(url = "{{ route('product.idex' }}") {
+            var keyword = $('#searchKeyword').val();
+            var company_id = $('#searchCompany').val();
+
+            $.ajax({
+                url: url,
+                type: "GET",
+                data: { prduct_name: keyword, company_id: company_id },
+                dataType: "json",
+                success: function(response) {
+                    var tableBody = $('#productTable tbody');
+                    tableBody.empty(); //一度テーブルをからにする
+
+                    if (response.html) {
+                        tableBody.append(response.html);
+                    } else {
+                        tableBody.append('<tr><td colspan="7" class="text-center py-4">該当する商品が見つかりません。</td></tr>');
+                    }
+
+                    //ページネーション部分を更新
+                    $('.pagination-wrapper').html(response.pagination);
+                },
+                error: function() {
+                    alert("検索に失敗しました")
+                }
+            });
+        }
+    });
+
     </script>
 @endsection
