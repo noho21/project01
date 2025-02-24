edbe92d7ab238fe4f084d57c27dc98f8217e6160
hono21 <0521hono@gmail.com>
2025-02-24 14:35:11 +0900

    見た目を整える

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -10,22 +10,27 @@
 
     <h3 class="text-left text-2xl w-50 py-5 m-auto">商品一覧画面</h3>
 
-    <form id="searchForm" class="space-x-4 flex justify-center">
-        <input id="searchKeyword" class="w-2/6 h-10 rounded border-2" type="text" name="product_name" placeholder="検索キーワード" value="{{ request('product_name') }}">
-        <select id="searchCompany" class="w-2/6 h-10 rounded border-2 text-center" name="company_id">
-                <option value="">メーカーを選択</option>
-                @foreach ($companies as $company)
-                    <option value="{{ $company->id }}" @selected((string)request('company_id') === (string)$company->id)>
-                        {{$company->company_name}}
-                    </option>
-                @endforeach
-        </select>
-        <input type="number" id="minPrice" name="min_price" placeholder="最小価格">
-        <input type="number" id="maxPrice" name="max_price" placeholder="最大価格">
-        <input type="number" id="minStock" name="min_stock" placeholder="最小在庫">
-        <input type="number" id="maxStock" name="max_stock" placeholder="最大在庫">
-
-        <input type="submit" id="searchButton" class="w-1/6 h-10 bg-gray-300 outline-offset-2 focus:ring-2 shadow-xl rounded-md" value="検索">
+    <form id="searchForm">
+        <div class="flex justify-center space-x-3">
+            <input id="searchKeyword" class="w-2/6 h-10 rounded border-4 border-green-600" type="text" name="product_name" placeholder="検索キーワード" value="{{ request('product_name') }}">
+            <select id="searchCompany" class="w-2/6 h-10 rounded border-4 border-green-600 text-center" name="company_id">
+                    <option value="">メーカーを選択</option>
+                    @foreach ($companies as $company)
+                        <option value="{{ $company->id }}" @selected((string)request('company_id') === (string)$company->id)>
+                            {{$company->company_name}}
+                        </option>
+                    @endforeach
+            </select>
+        </div>
+        <div class="flex justify-center space-x-3 py-3">
+            <input type="number" id="minPrice" class="border-2 border-pink-300 rounded-md text-left" name="min_price" placeholder="最小価格">
+            <input type="number" id="maxPrice" class="border-2 border-pink-400 rounded-md text-left" name="max_price" placeholder="最大価格">
+            <input type="number" id="minStock" class="border-2 border-pink-300 rounded-md text-left" name="min_stock" placeholder="最小在庫">
+            <input type="number" id="maxStock" class="border-2 border-pink-400 rounded-md text-left" name="max_stock" placeholder="最大在庫">
+        </div>
+        <div class="flex justify-center">
+            <input type="submit" id="searchButton" class="w-1/6 h-10 bg-gray-300 outline-offset-2 focus:ring-2 shadow-xl rounded-md" value="検索">
+        </div>
     </form>
 
     <div id="productList">
